4b7737adc81c59cd40f83cdc4e636ab06aa3835f
21paxa01 <20012005pk@gmail.com>
2021-09-02 16:13:25 +0300

    .

--- MyGame/Assets/scripts/money.cs
+++ MyGame/Assets/scripts/money.cs
@@ -7,31 +7,27 @@ public class money : MonoBehaviour
     public Rigidbody2D rb;
     public int test = 0;
     public Animator anim;
+    public Collider2D coll;
     void Start()
     {
         rb= GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        coll = GetComponent<Collider2D>();
     }
-
-    // Update is called once per frame
+    private bool drop = false;
     void Update()
     {
-        
+        if(transform.position.y<= -3.011614f)
+        {
+            Drop();
+        }
     }
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.name == "road u")
-        {
-
-            anim.SetBool("drop", true);
-            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-            rb.gravityScale = 0f;
-            rb.constraints = RigidbodyConstraints2D.None;
-
-
-        }
+        
         if (other.CompareTag("Player"))
         {
+            coll.isTrigger = true;
             test++;
            // rb.gravityScale = 0.1f;
             if (test > 1)
@@ -40,6 +36,19 @@ public class money : MonoBehaviour
                 MoneyCount.mon++;
             }
         }        
+    }
+    private int testik = 0;
+    void Drop()
+    {
+        if (testik == 0)
+        {
+            anim.SetBool("drop", true);
+            rb.constraints = RigidbodyConstraints2D.FreezePositionY;
+            rb.gravityScale = 0f;
+            rb.constraints = RigidbodyConstraints2D.None;
+            coll.isTrigger = true;
+            testik++;
+        }
     }
     
 }
